2f803b5b02303a11cf6a18d306cbae1de127c970
Vuppu Venkata Harika <131738315+Harika11114141@users.noreply.github.com>
2025-06-27 17:26:05 +0530

    Delete src directory

--- src/data.js
+++
@@ -1,6 +0,0 @@
-export const navLinks = [
-  { id: 1, text: 'Home', url: '#' },
-  { id: 2, text: 'About', url: '#' },
-  { id: 3, text: 'Services', url: '#' },
-  { id: 4, text: 'Contact', url: '#' },
-];
